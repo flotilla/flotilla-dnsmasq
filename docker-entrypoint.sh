7e3b503fe1d3fe1eff76f7146b8d8ebf1b8cd70c
Craig Tracey <craigtracey@gmail.com>
2018-01-21 12:28:21 -0500

    Replace sighup with full restart
    Looks like some config will not actually reload by sending a sighup to
    dnsmasq. Instead, do a full reload for now.
    
    Signed-off-by: Craig Tracey <craigtracey@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 dnsmasq -k --log-facility=- &
 while inotifywait -e close_write --timefmt '%H:%M' --format '%T %w %e %f' -r /etc/dnsmasq.d; do
 	PID=$(cat /var/run/dnsmasq.pid)
-	kill -1 ${PID}
+	if [ ! -z "$PID" ]; then
+		kill "${PID}"
+	fi
+	dnsmasq -k --log-facility=- &
 done
